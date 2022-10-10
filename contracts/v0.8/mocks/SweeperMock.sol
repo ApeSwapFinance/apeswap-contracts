8b55af5b398d6bf563cb45a1ca3f9db48df93962
defifofum <defifofum0@gmail.com>
2022-10-10 13:13:32 -0500

    refactor: Sweeper structure

--- contracts/v0.8/mocks/SweeperMock.sol
+++ contracts/v0.8/mocks/SweeperMock.sol
@@ -1,17 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-only
 pragma solidity ^0.8.0;
 
-/*
- * ApeSwapFinance
- * App:             https://apeswap.finance
- * Medium:          https://ape-swap.medium.com
- * Twitter:         https://twitter.com/ape_swap
- * Telegram:        https://t.me/ape_swap
- * Announcements:   https://t.me/ape_swap_news
- * GitHub:          https://github.com/ApeSwapFinance
- */
-
-import "../utils/Sweeper.sol";
+import "../token/Sweeper.sol";
 
 contract SweeperMock is Sweeper {
     address[] empty;
