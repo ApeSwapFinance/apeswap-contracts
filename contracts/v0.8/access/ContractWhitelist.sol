e6d5ce8d9e8e6d1b511433d40833af6c59e57b4f
defifofum <defifofum0@gmail.com>
2022-10-25 11:58:27 -0300

    refactor: Improve ContractWhitelist function visibility

--- contracts/v0.8/access/ContractWhitelist.sol
+++ contracts/v0.8/access/ContractWhitelist.sol
@@ -16,7 +16,7 @@ pragma solidity ^0.8.0;
           \▓▓                                              \▓▓         
  * App:             https://ApeSwap.finance
  * Medium:          https://ape-swap.medium.com
- * Twitter:         https://twitter.com/ape_swap 
+ * Twitter:         https://twitter.com/ape_swap
  * Telegram:        https://t.me/ape_swap
  * Announcements:   https://t.me/ape_swap_news
  * Discord:         https://discord.com/ApeSwap
@@ -47,7 +47,7 @@ abstract contract ContractWhitelist is IContractWhitelist, Ownable {
     }
 
     /// @notice enables smart contract whitelist
-    function toggleWhitelist() external override onlyOwner {
+    function toggleWhitelist() external virtual onlyOwner {
         whitelistEnabled = !whitelistEnabled;
         emit UpdateWhitelistStatus(whitelistEnabled);
     }
@@ -55,18 +55,14 @@ abstract contract ContractWhitelist is IContractWhitelist, Ownable {
     /// @notice Enable or disable a contract address on the whitelist
     /// @param _address Address to update on whitelist
     /// @param _enabled Set if the whitelist is enabled or disabled
-    function setContractWhitelist(address _address, bool _enabled) external override onlyOwner {
+    function setContractWhitelist(address _address, bool _enabled) external onlyOwner {
         _setContractWhitelist(_address, _enabled);
     }
 
     /// @notice Enable or disable contract addresses on the whitelist
     /// @param _addresses Addressed to update on whitelist
     /// @param _enabled Set if the whitelist is enabled or disabled for each address passed
-    function setBatchContractWhitelist(address[] memory _addresses, bool[] memory _enabled)
-        external
-        override
-        onlyOwner
-    {
+    function setBatchContractWhitelist(address[] memory _addresses, bool[] memory _enabled) external onlyOwner {
         require(_addresses.length == _enabled.length, "array mismatch");
         for (uint256 i = 0; i < _addresses.length; i++) {
             _setContractWhitelist(_addresses[i], _enabled[i]);
@@ -76,7 +72,7 @@ abstract contract ContractWhitelist is IContractWhitelist, Ownable {
     /// @notice Enable or disable a contract address on the whitelist
     /// @param _address Address to update on whitelist
     /// @param _enabled Set if the whitelist is enabled or disabled
-    function _setContractWhitelist(address _address, bool _enabled) private {
+    function _setContractWhitelist(address _address, bool _enabled) internal virtual {
         whitelist[_address] = _enabled;
         emit UpdateContractWhitelist(_address, _enabled);
     }
